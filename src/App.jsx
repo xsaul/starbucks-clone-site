220006c85df6e30fbe6341fd4b5ae8984b37735a
xsaul <saulglz0534@gmail.com>
2023-05-15 16:03:23 -0600

    home page changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Navbar from "../src/Components/Navbar"
+import Home from './Pages/Home'
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
      <Navbar />
+     <Home />
     </div>
   )
 }
